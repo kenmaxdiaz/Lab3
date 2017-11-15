51d71a6dc7ee8195654efb46d673b83712cb86ff
4010d7 <4010d7@guayaba.ece.uprm.edu>
2017-11-15 15:21:17 -0400

    Changes:
    Different colored borders, lines and an centralized oval were added to
    the program.

---
+++ Lab3/src/MyPanelClass.java
@@ -0,0 +1,48 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.JPanel;
+ 
+public class MyPanelClass extends JPanel {
+            /**
+	 * 
+	 */
+	private static final long serialVersionUID = 7057541440811488699L;
+
+			public void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+ 
+                        //Compute interior coordinates
+                        Insets myInsets = this.getInsets();
+                        int x1 = myInsets.left;
+                        int y1 = myInsets.top;
+                        int x2 = getWidth() - myInsets.right - 1;
+                        int y2 = getHeight() - myInsets.bottom - 1;
+                        int width = x2 - x1;
+                        int height = y2 - y1;
+ 
+//                        //Paint the background
+//                        g.setColor(Color.BLACK);
+//                        g.fillRect(x1, y1, width + 1, height + 1);
+//                        
+//                      //Draw a border
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1, y1, width, height);
+//                        
+//                      //Second Border
+//                        g.setColor(Color.CYAN);
+//                        g.drawRect(x1 + 4, y1 + 4, width - 8, height - 8);
+//                        
+//                      //CrossOne
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                      //CrossTwo
+//                        g.setColor(Color.RED);
+//                        g.drawLine(x2, y1, x1, y2);
+                        
+                        //Oval
+                          g.setColor(Color.LIGHT_GRAY);
+                          g.fillOval(getWidth()/2 - 55/2,getHeight()/2 - 55/2 , 55, 55);
+            }
+}
